73118e26899e6f7ed19adaaff9b2009a913d59af
yukino-mgf <xy182682@gmail.com>
2021-03-09 00:35:14 +0800

    2017-03-08

--- mangaspider.py
+++ mangaspider.py
@@ -4,5 +4,14 @@ import requests
 
 url = "https://copymanga.com"
 response = requests.get(url)
-with open("sample.html","wb") as f:
+with open("sample2.html","wb") as f:
     f.write(response.content)
+
+# result_urls = []
+result_urls = re.findall(r'href="(.*?)"',response.text)
+result_urls+=re.findall(r'data-src="(.*?)"',response.text)
+result_urls+=re.findall(r'url[(]"(.*?)"[)]',response.text)
+with open('./log/url.txt','w') as f:
+    for response_url in result_urls:
+        f.write(response_url+'\n')
+print(result_urls)
